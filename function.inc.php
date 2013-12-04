b8cedf88e9793552de54164a24c81a76018f3c5d
chnateag <chnateag@gmail.com>
2013-12-04 16:05:36 +0100

    ajout breadcrumb + header + icone extension

--- function.inc.php
+++ function.inc.php
@@ -4,20 +4,19 @@ function genererLigne($dir, $file){
 	$entry = $dir . $file;
 	if($file != '..' AND $file != '.'){
 		
-		echo '<tr>';
-		echo '<td>'. filetype($entry) . '</td>';
-		echo '<td><a href="?value='.urlencode($entry).'" class="tablelink">'.$file.'</a></td>';
-		echo '<td><a href="?ln='.$dir . $file.'"><button type="button" class="tablebtn" style="">Synchro</button></a></td>';
-		echo '</tr>';
+		echo '<tr>'."\n";
+		// echo ."\n";
+		echo '<td>',afficherIcone($entry),'<a href="?value='.urlencode($entry).'" class="tablelink">'.$file.'</a></td>'."\n";
+		echo '<td><a href="?ln='.$dir . $file.'"><button type="button" class="tablebtn" style="">Synchro</button></a></td>'."\n";
+		echo '</tr>'."\n\n";
 	}
         
 }
-
 function remonterDossier($dir){
 	$entry = $dir;
-	if($dir != "./"){
+	if($dir != "dl/"){
 		$arbo = explode("/",$entry);
-		// echo 'dir : '.$dir.'<br />';
+		// echo 'dir : '.$dir.'<br />'."\n";
 		$nombreClé = count($arbo)-2; // moins un dossier pour remonter + 1 compté par le explode apres le dernier slash
 		$i=0;
 		$dossierSup = "";
@@ -25,17 +24,80 @@ function remonterDossier($dir){
 			if($i < $nombreClé){ if($dossierSup == ""){ $dossierSup = $dossierSup.$value;} else { $dossierSup = $dossierSup."/".$value;}}
 			$i++;
 		}
-		// echo 'dossierSup : '.$dossierSup.'<br/>';;
-		// echo '<br />arbo : ';
-		// foreach($arbo as $key => $part){ echo $part.';';}
-		// echo '<br />';echo '<br />';
-		echo '<tr>';
-		echo '<td></td>';
-		echo '<td><a href="?value='.urlencode($dossierSup).'">Remonter vers '.$dossierSup.'</a></td>';
-		echo '<td></td>';
-		echo '</tr>';
+		// echo 'dossierSup : '.$dossierSup.'<br/>'."\n";;
+		// echo '<br />arbo : '."\n";
+		// foreach($arbo as $key => $part){ echo $part.'."\n";'."\n";}
+		// echo '<br />'."\n";echo '<br />'."\n";
+		echo '<tr>'."\n";
+		// echo '<td></td>'."\n";
+		echo '<td><img src="img/folder-parent.png" class="tableimg"/><a href="?value='.urlencode($dossierSup).'" class="tablelink">Remonter vers '.$dossierSup.'</a></td>'."\n";
+		echo '<td></td>'."\n";
+		echo '</tr>'."\n";
 	}
         
 }
 
+function afficherIcone($entry){
+	include('extension.php');
+	if(is_dir($entry)){
+		echo '<img src="img/folder.png" class="tableimg"/>';	// Afficher l'icone de dossier
+	}
+	else{
+		$explodeExtension = explode(".",$entry);
+		$nombreExplode = count($explodeExtension)-1;
+		if(is_array($arrayExtension)){
+			$find_ext=false;
+			foreach($arrayExtension as $key => $ext){ // parcours des groupes d'extension 
+					// echo '${$key} : Array | $key : '.$key.'<br />';
+						// print_r(${$key});
+				if(is_array(${$key})){
+					foreach(${$key} as $type => $temp){ // parcours des extensions dans les array groupes (video = mkv, avi, mp4)
+						
+						if($temp == $explodeExtension[$nombreExplode]){
+							echo '<img src="img/'.$arrayExtension[$key].'.png" class="tableimg"/>';
+							$find_ext=true;
+						}
+						
+					}
+				}
+				else{
+					echo '$key vide';
+				}
+			}
+			if(empty($find_ext)){
+				echo '<img src="img/default.png" class="tableimg"';
+			}
+		}
+		else{
+			echo 'arrayExtension vide';
+		}
+		// if(isset($find_ext[$key])){
+			// echo '<td><img src="img/default.png" style="width:16px;"/></td>'."\n";
+		// }
+	} 
+
+}
+
+
+
+function breadcrumb($arbo){
+	$arrayArbo = explode("/",$arbo);
+	$count = count($arrayArbo) -1;
+	// echo $count;
+	$dir = "";
+	echo '<ol class="breadcrumb">';
+	for($i = 0;$i < $count; $i++){
+		if($dir == ""){
+			$dir = $arrayArbo[$i];
+		}
+		else{
+			$dir = $dir . "/" . $arrayArbo[$i];
+		}
+		echo '<li><a href="?value='.urlencode($dir).'"';
+		if($i == $count-1) echo ' class="active"';
+		echo '>'.$arrayArbo[$i].'</a></li>';
+	}
+	echo '</ol>'."\n";
+
+}
 ?>
--- navig.php
+++ navig.php
@@ -22,7 +22,64 @@
 <!-- Include all compiled plugins (below), or include individual files as needed -->
 <script src="js/bootstrap.min.js"></script>
 
+
+
 <div class="container">
+
+
+<nav class="navbar navbar-default navbar-fixed-top navbar-inverse" role="navigation">
+  <!-- Brand and toggle get grouped for better mobile display -->
+  <div class="navbar-header">
+    <button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1">
+      <span class="sr-only">Toggle navigation</span>
+      <span class="icon-bar"></span>
+      <span class="icon-bar"></span>
+      <span class="icon-bar"></span>
+    </button>
+    <a class="navbar-brand" href="#">Gestion Download</a>
+  </div>
+
+  <!-- Collect the nav links, forms, and other content for toggling -->
+  <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
+    <ul class="nav navbar-nav">
+      <li class="active"><a href="#">Link</a></li>
+      <li><a href="#">Link</a></li>
+      <!--<li class="dropdown">
+        <a href="#" class="dropdown-toggle" data-toggle="dropdown">Dropdown <b class="caret"></b></a>
+        <ul class="dropdown-menu">
+          <li><a href="#">Action</a></li>
+          <li><a href="#">Another action</a></li>
+          <li><a href="#">Something else here</a></li>
+          <li class="divider"></li>
+          <li><a href="#">Separated link</a></li>
+          <li class="divider"></li>
+          <li><a href="#">One more separated link</a></li>
+        </ul>
+      </li>-->
+    </ul>
+    <!--<form class="navbar-form navbar-left" role="search">
+      <div class="form-group">
+        <input type="text" class="form-control" placeholder="Search">
+      </div>
+      <button type="submit" class="btn btn-default">Submit</button>
+    </form>-->
+    <!--<ul class="nav navbar-nav navbar-right">
+      <li><a href="#">Link</a></li>
+      <li class="dropdown">
+        <a href="#" class="dropdown-toggle" data-toggle="dropdown">Dropdown <b class="caret"></b></a>
+        <ul class="dropdown-menu">
+          <li><a href="#">Action</a></li>
+          <li><a href="#">Another action</a></li>
+          <li><a href="#">Something else here</a></li>
+          <li class="divider"></li>
+          <li><a href="#">Separated link</a></li>
+        </ul>
+      </li>
+    </ul>-->
+  </div><!-- /.navbar-collapse -->
+</nav>
+
+<div style="margin-top:60px;"></div> <!-- header de 46 px -->
 
 <?php
 include('function.inc.php');
@@ -33,24 +90,28 @@ if(isset($_GET['value'])){
 	if (is_dir($dir)){
 		$dir = $dir."/";
 	}
-	echo $dir;
+	// echo $dir;
 }
 else{
-	$dir = "./";
+	$dir = "dl/";
 	// $dir = ".";
 }
 
-echo '<br /><br />';
 
-echo '<table class="table table-hover table-condensed table-striped">';
-echo '<tbody>';
-echo '<thead>';
-echo '<tr>';
-echo '<th>type</th>';
-echo '<th>nom</th>';
-echo '<th>synchro</th>';
-echo '</tr>';
-echo '</thead>';
+breadcrumb($dir);
+
+
+echo '<br /><br />'."\n";
+
+echo '<table class="table table-hover table-condensed table-striped">'."\n";
+echo '<tbody>'."\n";
+echo '<thead>'."\n";
+echo '<tr>'."\n";
+// echo '<th>type</th>'."\n";
+echo '<th>nom</th>'."\n";
+echo '<th><span style="float:right;">synchro</span></th>'."\n";
+echo '</tr>'."\n";
+echo '</thead>'."\n";
 // Ouvre un dossier bien connu, et liste tous les fichiers
 if (is_dir($dir)){
 	
@@ -63,8 +124,8 @@ if (is_dir($dir)){
         closedir($dh);
     }
 }
-echo '</tbody>';
-echo '</table>';
+echo '</tbody>'."\n";
+echo '</table>'."\n";
 ?>
 </div>
 </body>
